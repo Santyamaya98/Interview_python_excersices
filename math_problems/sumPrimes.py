06eccc62a1021a759af400c7813bb5faeb026fd9
Santiago Amaya <santyamaya.98.3.12@gmail.com>
2025-11-27 21:43:42 +0100

    sumPrimes.py

--- math_problems/sumPrimes.py
+++ math_problems/sumPrimes.py
@@ -8,7 +8,7 @@ class Solution:
             if num % 2 == 0 or num % 3 == 0:
                 return False
             i = 5
-            while i * i <= num:
+            while i * i < num:
                 if num % i == 0 or num % (i + 2) == 0:
                     return False
                 i += 6
