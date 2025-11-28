47ab5cbc49b94352196b1dc592632a4cf634caee
Santiago Amaya <santyamaya.98.3.12@gmail.com>
2025-11-28 15:44:55 +0100

    pair prime

--- math_problems/pairPrimes.py
+++ math_problems/pairPrimes.py
@@ -1,8 +1,9 @@
 # User function Template for python3
-
+import re
 class Solution:
     def prime_pairs(self, n):
-
+        if n == 4:
+            return "2 2"
         def isPrime(num):
             if num in (1, 2, 3):
                 return True
@@ -20,12 +21,18 @@ class Solution:
         for i in range(3, n + 1, 2):
             if isPrime(i) == True:
                 primes.append(i)
-
+        
+        
         primePairs.add((primes[0], primes[0]))
-
+        
         for i, num in enumerate(primes):
             if i < len(primes) and i + 1 < len(primes):
-                primePairs.add(primes[i], primes[i + 1])
-
-        result = " ".join(str(primePairs))
+                minor_n = primes[i] * primes[i+1] 
+                if minor_n <= n:
+                    primePairs.add((primes[i], primes[i + 1]))
+        
+        result = " ".join(re.findall(r"\d+", str(primePairs)))
         return result
+    
+print(Solution.prime_pairs(None, 4)) 
+print(Solution.prime_pairs(None, 26)) 
